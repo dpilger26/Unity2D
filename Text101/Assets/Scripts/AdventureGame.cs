026851cbe0b0eab2dce9f7e17fd4e047c6d0bf9c
David Pilger <dpilger26@gmail.com>
2019-02-04 23:57:58 -0700

    Adding Text101

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Text
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent; // [SerializeField] adds this to the game component inside of Unity
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            textComponent.text = state.GetStateStory();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            textComponent.text = state.GetStateStory();
+        }
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [CreateAssetMenu(menuName = "State")] creates a 'State' choice under asset menu -> create -> 'State'
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // [SerializeField] adds the field to the inspector within unity
+    // [TextArea(14, 10)] changes the size of the text field within unity so that we can 
+    // actually see what we are typing
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
