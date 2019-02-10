1bfda47a90b691d3c22e3b72878c4163c6c55e3a
David Pilger <dpilger26@gmail.com>
2019-02-09 18:04:58 -0700

    Lecture 132

---
+++ Glich Garden/Assets/Scripts/Defender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    // configuration parameter
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ThrowProjectile()
+    {
+
+    }
+}
