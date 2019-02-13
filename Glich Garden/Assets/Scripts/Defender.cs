1c9e865a2fed419e8d9ab56a92b1a9427740f4cf
David Pilger <dpilger26@gmail.com>
2019-02-12 22:07:01 -0700

    Lecture 147

--- Glich Garden/Assets/Scripts/Defender.cs
+++ Glich Garden/Assets/Scripts/Defender.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
+    //  configurable parameters
     [SerializeField] int starCost = 100;
+
+    // cached parameters
+    StarDisplay starDisplay;
+
+    private void Update()
+    {
+        starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    public void AddStars(int numStars)
+    {
+        starDisplay.AddStars(numStars);
+    }
 }
--- Glich Garden/Assets/Scripts/Projectile.cs
+++ Glich Garden/Assets/Scripts/Projectile.cs
@@ -6,14 +6,12 @@ public class Projectile : MonoBehaviour
 {
     // state parameters
     [SerializeField] float speed = 1f;
-    //[SerializeField] float rotationRate = 360f;
     [SerializeField] int damage = 100;
 
     // Update is called once per frame
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        //transform.Rotate(new Vector3(0, 0, rotationRate * Time.deltaTime));
 
         if (transform.position.x > 12)
         {
@@ -25,11 +23,6 @@ public class Projectile : MonoBehaviour
     {
         speed = newSpeed;
     }
-
-    //public void SetRotationRate(float newRotationRate)
-    //{
-    //    rotationRate = newRotationRate;
-    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
