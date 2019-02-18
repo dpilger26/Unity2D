996d814d65ff4e436f1752b1f31bb7c0de6d3d21
David Pilger <dpilger26@gmail.com>
2019-02-17 17:41:21 -0700

    Lecture 161

---
+++ Glich Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    // configurable parameters
+    [Tooltip("Our Level Timer in secodns")]
+    [SerializeField] float levelTime = 10f;
+
+    // cached parameters
+    Slider timeSlider;
+
+    private void Start()
+    {
+        timeSlider = GetComponent<Slider>();
+
+        UpdateSlider();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSlider();
+
+        if (LevelFinished())
+        {
+            Debug.Log("Level Finished");
+        }
+    }
+
+    void UpdateSlider()
+    {
+        timeSlider.value = Time.timeSinceLevelLoad / levelTime;
+    }
+
+    bool LevelFinished()
+    {
+        return Time.timeSinceLevelLoad >= levelTime ? true : false;
+    }
+}
