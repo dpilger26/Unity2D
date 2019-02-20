580dcf8cd3f771193dafcfe9e43ec743e4680965
David Pilger <dpilger26@gmail.com>
2019-02-19 20:16:58 -0700

    Lecture 167

---
+++ Glich Garden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // cashed parameters
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FindObjectsOfType<MusicPlayer>().Length == 1)
+        {
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
---
+++ Glich Garden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] [Range(0, 1)] float defaultVolume = 0.8f;
+    [SerializeField] Slider difficultySlider;
+    [SerializeField] [Range(0, 1)] float defaultDifficulty = 0.5f;
+
+    // cashed references
+    MusicPlayer musicPlayer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+        difficultySlider.value = PlayerPrefsController.GetDifficulty();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        musicPlayer.SetVolume(volumeSlider.value);
+    }
+
+    public void SetDefaults()
+    {
+        PlayerPrefsController.SetMasterVolume(defaultVolume);
+        volumeSlider.value = defaultVolume;
+
+        PlayerPrefsController.SetDifficulty(defaultDifficulty);
+        difficultySlider.value = defaultDifficulty;
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsController.SetDifficulty(difficultySlider.value);
+        FindObjectOfType<LevelLoader>().LoadStartMenu();
+    }
+}
