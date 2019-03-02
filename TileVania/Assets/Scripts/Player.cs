ea6ba0385ab1c878f996aad02b9d251f047fef30
David Pilger <dpilger26@gmail.com>
2019-03-02 15:06:19 -0700

    Lecture 189

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float runSpeed = 5f;
+
+    // cached references
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        var deltaTime = Time.deltaTime * runSpeed; // NOTE: makes things time independent
+        var deltaX = Input.GetAxis("Horizontal") * deltaTime;
+
+        if (Mathf.Abs(deltaX) < Mathf.Epsilon)
+        {
+            // not moving
+            return;
+        }
+
+        UpdateSpriteDirection(deltaX);
+        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y);
+    }
+
+    private void UpdateSpriteDirection(float deltaX)
+    {
+        spriteRenderer.flipX = deltaX < 0 ? true : false;
+    }
+}
