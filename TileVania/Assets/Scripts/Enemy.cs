3332e03503ca76e5b939598ffc10cca5b4e19f87
David Pilger <dpilger26@gmail.com>
2019-03-06 19:20:02 -0700

    Lecture 201

--- TileVania/Assets/Scripts/Enemy.cs
+++ TileVania/Assets/Scripts/Enemy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // configuration parameters
+    [SerializeField] float moveSpeed = 1f;
+
+    // cached references
+    Rigidbody2D myRigidBody;
+
+    private void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(myRigidBody.velocity.x), 1f);
     }
 }
--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -34,7 +34,6 @@ public class Player : MonoBehaviour
         beginningGravityScale = myRigidBody.gravityScale;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         Run();
