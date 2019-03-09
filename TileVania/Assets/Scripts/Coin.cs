bc68f2da3469a93edcfc7bbc46bd0042b876608c
David Pilger <dpilger26@gmail.com>
2019-03-08 20:51:42 -0700

    Lecture 208

---
+++ TileVania/Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupClip;
+    [SerializeField] [Range(0, 1)] float coinPickupVolume = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ConsumeCoin();
+        }
+    }
+
+    private void ConsumeCoin()
+    {
+        AudioSource.PlayClipAtPoint(coinPickupClip, Camera.main.transform.position, coinPickupVolume);
+        Destroy(gameObject);
+    }
+}
